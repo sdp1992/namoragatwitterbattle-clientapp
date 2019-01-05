c4f8a55ae4cf0d0fc91ffafe43000877ea6dd568
sdp1992 <soumyadipdutta2007@gmail.com>
2019-01-06 02:04:50 +0530

    NA

--- app.py
+++ app.py
@@ -4,46 +4,121 @@ import ast
 
 app = Flask(__name__)
 
-labels = [1,2,3,4,5,6,7,8,9,10]
-namo_values = [1,2,3,4,5,6,7,8,9,10]
-raga_values = [1,2,3,4,5,6,7,8,9,10]
+
+def initialize_value(return_params):
+        count_values = []
+        for i in range(360):
+            count_values.append(0)
+        labels_values = []
+        j = 6
+        for i in range(360):
+            if i in [0,60,120,180,240,300]:
+                labels_values.append("-" + str(10*j) + " Min")
+                j = j-1
+            else:
+                labels_values.append("")
+        if return_params == "labels":
+            return labels_values
+        else:
+            return count_values
+
+
+labels1 = initialize_value("labels")
+labels2 = initialize_value("labels")
+
+namo_count = initialize_value("count")
+raga_count = initialize_value("count")
+
+namo_positive = initialize_value("count")
+raga_positive = initialize_value("count")
+
+namo_negative = initialize_value("count")
+raga_negative = initialize_value("count")
 
 
 @app.route("/")
 def chart():
-    global labels, namo_values, raga_values
-    labels = [1,2,3,4,5,6,7,8,9,10]
-    namo_values = [1,2,3,4,5,6,7,8,9,10]
-    raga_values = [1,2,3,4,5,6,7,8,9,10]
-    return render_template('index.html', labels=labels, namo_values=namo_values, raga_values=raga_values)
+    global labels1, labels2, namo_count, raga_count, namo_positive, namo_negative, raga_positive, raga_negative
+
+    return render_template('index.html', labels1=labels1, labels2=labels2,
+                           namo_count=namo_count, raga_count=raga_count,
+                           namo_positive = namo_positive, raga_positive = raga_positive,
+                           namo_negative = namo_negative, raga_negative = raga_negative)
 
 
 @app.route('/refreshData')
 def refresh_graph_data():
-    global labels, namo_values, raga_values
-    print("labels now: " + str(labels))
-    print("Namo data now: " + str(namo_values))
-    print("Raga data now: " + str(raga_values))
-    return jsonify(sLabel=labels, sNamoData=namo_values, sRagaData = raga_values)
+    global labels1, labels2, namo_count, raga_count, namo_positive, namo_negative, raga_positive, raga_negative
+
+    # print("labels1 now: " + str(labels1))
+    # print("labels2 now: " + str(labels2))
+    # print("Namo count now: " + str(namo_count))
+    # print("Raga count now: " + str(raga_count))
+    # print("Namo positive count now: " + str(namo_positive))
+    # print("Namo negative count now: " + str(namo_negative))
+    # print("Raga positive count now: " + str(raga_positive))
+    # print("Raga negative count now: " + str(raga_negative))
+
+    return jsonify(sLabel1=labels1, sLabel2=labels2, sNamoData=namo_count, sRagaData=raga_count,
+                   sNamoPositive=namo_positive, sNamoNegative=namo_negative,
+                   sRagaPositive=raga_positive, sRagaNegative=raga_negative)
 
 
 @app.route('/updateData', methods=['POST'])
 def update_data_post():
-    global labels, namo_values, raga_values
-    if not request.form or 'namo_data' not in request.form:
+    global labels1, namo_count, raga_count
+    if not request.form or 'namo_count' not in request.form:
         return "Error",400
 
-    labels = ast.literal_eval(request.form['label'])
-    namo_values.pop(0)
-    namo_values.append(ast.literal_eval(request.form['namo_data'])[0])
-    raga_values.pop(0)
-    raga_values.append(ast.literal_eval(request.form['raga_data'])[0])
-    print("labels received: " + str(labels))
-    print("Namo data received: " + str(namo_values))
-    print("Namo data received: " + str(raga_values))
+    #labels1 = ast.literal_eval(request.form['label'])
+
+    namo_count.pop(0)
+    namo_count.append(ast.literal_eval(request.form['namo_count'])[0])
+
+    raga_count.pop(0)
+    raga_count.append(ast.literal_eval(request.form['raga_count'])[0])
+
+    #print("labels received: " + str(labels1))
+    # print("Namo data received: " + str(namo_count))
+    # print("Namo data received: " + str(raga_count))
+
     return "success",201
 
 
+@app.route('/updateSentimentData', methods=['POST'])
+def update_data_post_sentiment():
+    global labels2, namo_positive, namo_negative, raga_positive, raga_negative
+    if not request.form or 'namo_positive' not in request.form:
+        return "Error",400
+
+    #labels2 = ast.literal_eval(request.form['label'])
+
+    namo_positive.pop(0)
+    namo_positive.append(ast.literal_eval(request.form['namo_positive'])[0])
+
+    namo_negative.pop(0)
+    namo_negative.append(ast.literal_eval(request.form['namo_negative'])[0])
+
+    raga_positive.pop(0)
+    raga_positive.append(ast.literal_eval(request.form['raga_positive'])[0])
+
+    raga_negative.pop(0)
+    raga_negative.append(ast.literal_eval(request.form['raga_negative'])[0])
+
+    # print("labels received: " + str(labels2))
+    # print("Namo positive count received: " + str(namo_positive))
+    # print("Namo negative count received: " + str(namo_negative))
+    # print("Raga positive count received: " + str(raga_positive))
+    # print("Raga negative count received: " + str(raga_negative))
+
+    return "success",201
+
+
+@app.route('/aboutme')
+def json():
+    return render_template('page1.html')
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5001)
 
---
+++ static/CustomScript.js
@@ -0,0 +1,198 @@
+
+ var tweetCanvas = document.getElementById("chart_count");
+ var tweetCanvasPositive = document.getElementById("chart_positive");
+ var tweetCanvasNegative = document.getElementById("chart_negative");
+
+    Chart.defaults.global.defaultFontFamily = "Georgia";
+    Chart.defaults.global.defaultFontSize = 18;
+    Chart.defaults.global.pointRadius = 1;
+    Chart.defaults.global.pointHoverRadius = 2;
+    //Chart.defaults.global.animation = 'linear';
+
+    var chartOptions = {
+        legend: {
+            display: true,
+            position: 'left',
+            labels: {
+                        boxWidth: 100,
+                        fontColor: 'black'
+            }
+        },
+        layout: {
+            padding: {
+                left: 20,
+                right: 20,
+                top: 50,
+                bottom: 50
+            }
+        },
+        scales: {
+            xAxes: [{
+
+                gridLines: {
+                    display:false
+                }
+
+            }],
+            yAxes: [{
+                gridLines: {
+                    display:true
+                },
+                ticks: {
+                    min: 0,
+                    stepSize: 5
+                }
+            }]
+        }
+    };
+
+    var lineChart = new Chart(tweetCanvas, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+            {
+                    label: "Narendra Modi",
+                    data: [],
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'rgb(224, 135, 80)',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'rgb(224, 135, 80)',
+                    pointBackgroundColor: 'black',
+                    pointHitRadius: 30,
+                    pointBorderWidth: 1
+            },
+            {
+                    label: "Rahul Gandhi",
+                    data: [],
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'rgb(67, 153, 219)',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'rgb(67, 153, 219)',
+                    pointBackgroundColor: 'black',
+                    pointHitRadius: 30,
+                    pointBorderWidth: 0
+            }
+            ]
+        },
+
+        options: chartOptions
+    });
+
+    var lineChartPositive = new Chart(tweetCanvasPositive, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+            {
+                    label: "Narendra Modi",
+                    data: [],
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'rgb(224, 135, 80)',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'rgb(224, 135, 80)',
+                    pointBackgroundColor: 'black',
+                    pointHitRadius: 30,
+                    pointBorderWidth: 1
+            },
+            {
+                    label: "Rahul Gandhi",
+                    data: [],
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'rgb(67, 153, 219)',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'rgb(67, 153, 219)',
+                    pointBackgroundColor: 'black',
+                    pointHitRadius: 30,
+                    pointBorderWidth: 1
+            }
+            ]
+        },
+
+        options: chartOptions
+    });
+
+    var lineChartNegative = new Chart(tweetCanvasNegative, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+            {
+                    label: "Narendra Modi",
+                    data: [],
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'rgb(224, 135, 80)',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'rgb(224, 135, 80)',
+                    pointBackgroundColor: 'black',
+                    pointHitRadius: 30,
+                    pointBorderWidth: 1
+            },
+            {
+                    label: "Rahul Gandhi",
+                    data: [],
+                    lineTension: 0.3,
+                    fill: false,
+                    borderColor: 'rgb(67, 153, 219)',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: 'rgb(67, 153, 219)',
+                    pointBackgroundColor: 'black',
+                    pointHitRadius: 30,
+                    pointBorderWidth: 1
+            }
+            ]
+        },
+
+        options: chartOptions
+    });
+
+    var src_Labels_1 = [];
+    var src_NamoData = [];
+    var src_RagaData = [];
+
+    var src_Labels_2 = [];
+    var src_NamoData_positive = [];
+    var src_RagaData_positive = [];
+
+    var src_NamoData_negative = [];
+    var src_RagaData_negative = [];
+
+    setInterval(function(){
+          $.getJSON('/refreshData', {
+            }, function(data) {
+
+               src_Labels_1 = data.sLabel1;
+               src_Labels_2 = data.sLabel2;
+
+               src_NamoData = data.sNamoData;
+               src_RagaData = data.sRagaData;
+
+               src_NamoData_positive = data.sNamoPositive;
+               src_RagaData_positive = data.sRagaPositive;
+
+               src_NamoData_negative = data.sNamoNegative;
+               src_RagaData_negative = data.sRagaNegative;
+
+           });
+
+           lineChart.data.labels = src_Labels_1;
+           lineChart.data.datasets[0].data = src_NamoData;
+           lineChart.data.datasets[1].data = src_RagaData;
+           lineChart.update();
+
+           lineChartPositive.data.labels = src_Labels_2;
+           lineChartPositive.data.datasets[0].data = src_NamoData_positive;
+           lineChartPositive.data.datasets[1].data = src_RagaData_positive;
+           lineChartPositive.update();
+
+           lineChartNegative.data.labels = src_Labels_2;
+           lineChartNegative.data.datasets[0].data = src_NamoData_negative;
+           lineChartNegative.data.datasets[1].data = src_RagaData_negative;
+           lineChartNegative.update();
+
+      },1000);
